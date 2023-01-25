97fe93d4b514180a46553f2c484eb409b4367e56
Dan-Heller <danheller@outlook.co.il>
2023-01-25 23:26:19 +0200

    fetching from server , first steps

--- src/components/ContentBrowse/ContentBrowse.js
+++ src/components/ContentBrowse/ContentBrowse.js
@@ -31,7 +31,7 @@ class ContentBrowse extends Component {
 
 
   componentDidMount() {
-    const movie1 = new movie("Doctor Strange in the Multiverse of Madness",'https://m.media-amazon.com/images/I/913v0sAkiyL._AC_UY218_.jpg');
+    /*const movie1 = new movie("Doctor Strange in the Multiverse of Madness",'https://m.media-amazon.com/images/I/913v0sAkiyL._AC_UY218_.jpg');
     const movie2 = new movie("Avengers",'https://m.media-amazon.com/images/I/81lMrc8bBRL._AC_UY218_.jpg');
     const movie3 = new movie("Captain America: The First Avenger",'https://m.media-amazon.com/images/I/91LmsuoBOIL._AC_UY218_.jpg');
     const movie4 = new movie("Iron Man",'https://m.media-amazon.com/images/I/91qvAndeVYL._AC_UY218_.jpg');
@@ -40,8 +40,17 @@ class ContentBrowse extends Component {
     const movie7 = new movie("Captain America : The Winter Soldier",'https://m.media-amazon.com/images/I/91cW92vUDoL._AC_UY218_.jpg');
     const movie8 = new movie("Thor : The Dark World",'https://m.media-amazon.com/images/I/91RsJltFGLL._AC_UY218_.jpg');
     const movie9 = new movie("Thor : Ragnarok",'https://m.media-amazon.com/images/I/81l1Kaw8aKL._AC_UY218_.jpg');
-    const fetchedTitles =  [movie1, movie2, movie3, movie4,movie5,movie6,movie7,movie8, movie9];
-    this.setState({titles : fetchedTitles })
+    const fetchedTitles =  [movie1, movie2, movie3, movie4,movie5,movie6,movie7,movie8, movie9];*/
+    fetch('http://localhost:3001/AllTitles')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ titles: data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+    /*this.setState({titles : fetchedTitles })*/
   }
 
 
